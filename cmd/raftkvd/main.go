c8e85d0b39ad7b6e6cde1bb9c203f4fd02ede216
samithreddychinni <samithreddy067@gmail.com>
2026-04-16 15:03:15 +0530

    feat(wal): integreate write-ahead logging for store persistence and recovery

--- cmd/raftkvd/main.go
+++ cmd/raftkvd/main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/samithreddychinni/raftkv/internal/store"
 	"github.com/samithreddychinni/raftkv/server"
 )
 
+const walPath = "raftkvd.wal"
+
 func main() {
-	s := store.NewStore()
+	s, err := store.NewStoreFromWAL(walPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open store: %v\n", err)
+		os.Exit(1)
+	}
+	defer s.Close()
+
 	srv := server.NewServer(s)
 	fmt.Println("RaftKV Server starting on :8080...")
 	if err := http.ListenAndServe(":8080", srv); err != nil {
